19b893cd0ea472023a6c60c725ce4b725d099f43
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-12-07 17:15:16 +0900

    Title: 윤년인가, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231207/윤년인가/is-leap-year.py
@@ -0,0 +1,12 @@
+y = int(input())
+
+if y % 4 ==0:
+    if y % 4 ==0 and y % 100 == 0:
+        print('false')
+    elif y % 4 == 0 and y % 100 == 0 and y % 400 == 0:
+        print('true')
+    else:
+        print('true')
+
+else:
+    print('false')
