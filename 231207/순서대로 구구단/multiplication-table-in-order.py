1b3f89ae7aa31e90fa51232a77680e9d67158b3d
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-12-07 16:57:54 +0900

    Title: 순서대로 구구단, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231207/순서대로 구구단/multiplication-table-in-order.py
+++ 231207/순서대로 구구단/multiplication-table-in-order.py
@@ -1,8 +1,9 @@
 a, b = map(int, input().split())
 
 r = 1 if a < b else -1
+e = b+1 if a < b else b-1
 
 for i in range(1, 10):
-    for j in range(a, b+1, r):
+    for j in range(a, b, r):
         print(f'{j} * {i} = {j*i}', end="  ")
     print()
