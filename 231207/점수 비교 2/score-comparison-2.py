ef5fdd7722f4a050fea9427a01d129905f6ed9c1
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-12-07 16:34:08 +0900

    Title: 점수 비교 2, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231207/점수 비교 2/score-comparison-2.py
+++ 231207/점수 비교 2/score-comparison-2.py
@@ -1,5 +1,5 @@
-A = list(map(int, input().split))
-B = list(map(int, input().split))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
 if A[0] > B[0] and A[1] > B[1]:
     print(1)
