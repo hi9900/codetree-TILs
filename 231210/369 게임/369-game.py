d3a8303195d44e9d29b919b636705ac370495d5f
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-12-10 17:15:07 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231210/369 게임/369-game.py
+++ 231210/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0 or str(i) in '3' or str(i) in '6' or str(i) in '9':
+    if i % 3 == 0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
         print(0, end=" ")
     else:
         print(i, end=" ")
