4c9ecef51e0d0b1e3db9f294a41a3ccbcd688d89
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-12-12 13:40:42 +0900

    Title: 청소당번 정하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231212/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+x, y, z = 0, 0, 0
+
+for i in range(1, n+1):
+    if i % 12 == 0:
+        z += 1
+    elif i % 3 == 0:
+        y += 1
+    elif i % 2 == 0:
+        x += 1
+
+print(x, y, z)
